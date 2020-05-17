76902bab30eeaff829958c353565a5a8bf491340
Nehliin <oskar.d.nehlin@gmail.com>
2020-05-17 15:07:15 +0200

    reexport structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
+pub mod render_node;
 pub mod shader;
+pub mod texture;
 pub mod uniforms;
 pub mod vertex_buffers;
-pub mod texture;
-pub mod render_node;
 
 use std::path::PathBuf;
 use thiserror::Error;
 
+pub use render_node::{RenderNode, RenderNodeBuilder};
+pub use shader::{FragmentShader, FragmentShaderBuilder, VertexShader, VertexShaderBuilder};
+pub use texture::{SimpleTexture, Texture};
+pub use uniforms::{UniformBindGroup, UniformBindGroupBuilder};
 pub unsafe trait GpuData: 'static {
     fn as_raw_bytes(&self) -> &[u8];
 }
@@ -17,7 +21,6 @@ pub trait VertexBufferData: GpuData {
 
 #[derive(Error, Debug)]
 pub enum RenderError {
-    
     #[error("Couldn't compile shader file {path:?}: {compile_error:?}")]
     ShaderCompileError {
         compile_error: String,
@@ -29,13 +32,13 @@ pub enum RenderError {
 
     #[error("Couldn't open image")]
     TextureLoadError(#[from] image::ImageError),
-    
+
     #[error("Issue with shader file")]
     ShaderFileError(#[from] std::io::Error),
-    
+
     #[error("GpuData can't be zero sized")]
     ZeroSizedGpuData,
-    
+
     #[error("There is already a binding for this GpuData in this bindgroup")]
     GpuDataTypeAlreadyPresent,
 
